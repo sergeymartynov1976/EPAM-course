65291df2e8f3c3d7e231c33f4c75d80dcccdf232
sergeymartynov1976 <49633353+sergeymartynov1976@users.noreply.github.com>
2020-07-08 18:08:59 +0300

    New version of home work after lesson 14
    Changed way of parsing parameters from comand line/

--- ls.py
+++ ls.py
@@ -1,6 +1,6 @@
-import logging
+import argparse
 import os
-import sys
+import logging
 
 
 NAME = 'ls emulation'
@@ -14,13 +14,12 @@ def print_string(path, args):
     """Печатает содержание папки в строку. Если передан параметр а, печатет и скрытые папки и файлы,
     если передан параметр d, печатает только папки, если передан параметр s, выводит размеры файлов и папок.
     :type path: str
-    :type args: str
     """
     items_raw = os.listdir(path)
     # Очищаем список элементов директории в зависимости от переданных параметров а и d.
     items = []
     for item in items_raw:
-        if 'd' in args and os.path.isdir(path + os.sep + item) is True:
+        if 'd' in args and os.path.isdir(path+os.sep+item) is True:
             items.append(item)
         elif 'd' not in args:
             items.append(item)
@@ -31,9 +30,9 @@ def print_string(path, args):
 
     # Функция печати элемента
     def print_item(item):
-        descr = '<Dir>' if os.path.isdir(path + os.sep + item) else '<File>'
+        descr = '<Dir>' if os.path.isdir(path+os.sep+item) else '<File>'
         if 's' in args:
-            print(f'{item}  {descr} {os.path.getsize(path + os.sep + item)} bites', end='    ')
+            print(f'{item}  {descr} {os.path.getsize(path+os.sep+item)} bites', end='    ')
         else:
             print(f'{item}  {descr}', end='    ')
 
@@ -41,14 +40,13 @@ def print_string(path, args):
         print_item(item)
     print(' ')
 
-
 # Функция вывода дерева директории
 def folder_tree(path):
     """
     Выводит дерево директории (каталога). Папка, файлы, вложенная папака, файлы и папки в ней и т.д.
 
     :param folder: Название папки, у которой нужно вывести дерево папок и фалов.
-    :type a: str
+    :type path: str
 
     """
     for root, directories, files in os.walk(path):  # Перебирает все уровни дерева каталога папок
@@ -57,8 +55,7 @@ def folder_tree(path):
         print(
             f'{otst}[{root.split(os.sep)[-1]}]   {os.path.getsize(root)} bites')  # Печатаем название папки с отступом, соответствующим уровню папки.
         for file in files:
-            print(
-                f'{otst}    {file}   {os.path.getsize(root + os.sep + file)} bites')  # Печатаем названия файлов в папке
+            print(f'{otst}    {file}   {os.path.getsize(root+os.sep+file)} bites')  # Печатаем названия файлов в папке
 
 
 # Логгер.
@@ -66,45 +63,34 @@ LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 logger = logging.getLogger()
 
+
 if __name__ == '__main__':
     # Считываем значения аргументов
-    if len(sys.argv) > 3:
-        print('Неправильный формат ввода')
-        sys.exit(-1)
+    p = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION, epilog=EPILOG, add_help=False)
+    p.add_argument('-a', action='store_true', help='Option to show hidden objects')
+    p.add_argument('-d', action='store_true', help='Option to show only directories')
+    p.add_argument('-s', action='store_true', help='Option to show sizes objects')
+    p.add_argument('-t', action='store_true', help='Option to show complete tree of directory')
+    p.add_argument('path', type=str, default='.', nargs='?', help='Path to directory to be processed')
+    p.add_argument('--help', '-h', action='help', help='Help message')
+    p.add_argument('--version', '-v', action='version', help='Version', version='%(prog)s {}'.format(VERSION))
+    args = p.parse_args()
 
-    if len(sys.argv) == 1:
-        path = '.'
-        params = '-'
-
-    if len(sys.argv) == 2:
-        if sys.argv[1][0] == '-':
-            path = '.'
-            params = sys.argv[1]
-        else:
-            path = sys.argv[1]
-            params = '-'
-    if len(sys.argv) == 3:
-        path = sys.argv[2]
-        params = sys.argv[1]
+    params = []
+    if args.a:
+        params.append('a')
+    if args.d:
+        params.append('d')
+    if args.s:
+        params.append('s')
+    if args.t:
+        params.append('t')
 
     logger.info('Параметры введены')
 
-    # Проверяем правильность введения параметров
-    if len(params) > 6:
-        print('Неправильный размер ключа')
-        sys.exit(-1)
-    if params[0] != '-':
-        print('Неверный формат ключа')
-        sys.exit(-1)
-    for param in params[1:]:
-        if param not in ['a', 'd', 's', 't']:
-            print('Неправильный элемент в ключе')
-            sys.exit(-1)
-
-    logger.info('Параметры проверены')
     # Основной процесс обработки
-    print_string(path, params)
+    print_string(args.path, params)
     if 't' in params:
-        folder_tree(path)
+        folder_tree(args.path)
 
     logger.info('Работа программы завершена')
